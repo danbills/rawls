6e88140e5bd0f7b39b95cbe175502c817e09400a
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2015-10-02 10:24:12 -0400

    fixes

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
+#add vagrant user to root group for docker access:
+gpasswd -a vagrant root
 
+#setup puppet
 sudo yum -y --nogpgcheck localinstall https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
 sudo yum -y install rubygems git puppet unzip
 gem install librarian-puppet
-#add github key
 
+#add github key
 sudo sh -c 'ssh-keyscan github.com >> /etc/ssh/ssh_known_hosts '
 
 cd /vagrant
 
+#fetch puppet modules
 /usr/local/bin/librarian-puppet install
 
+#run puppet
 sudo puppet apply --verbose --modulepath=modules/ default.pp
 
+#setup time
 sudo rm -f /etc/localtime
 sudo ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
+
 
 #App specific things go here (ie, copy a config to a place, etc etc)
 sudo cp -v site.conf /etc/
