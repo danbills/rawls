365147ab68e89aed4b52c3c1f9d1773b0d762ea5
Matt Bemis <mbemis@broadinstitute.org>
2015-12-01 14:17:38 -0500

    added 100 analysis test

---
+++ src/perf/launchAnalysis_100.scala
@@ -0,0 +1,31 @@
+package default
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class launchAnalysis100 extends Simulation {
+
+	val accessToken = "YOUR_ACCESS_TOKEN" //place your token here :)
+
+	val httpProtocol = http
+		.baseURL("https://rawls.dsde-dev.broadinstitute.org")
+		.inferHtmlResources()
+	//	.extraInfoExtractor(extraInfo => List(extraInfo.response)) //for when we want to extract additional info for the simulation.log
+
+	val headers = Map("Authorization" -> s"Bearer ${accessToken}",
+						"Content-Type" -> "application/json") 
+
+	val submissionBody = """{"methodConfigurationNamespace":"alex_methods","methodConfigurationName":"cancer_exome_pipeline_v2","entityType":"pair_set","entityName":"pair_set_1","expression":"this.pairs"}"""
+
+	val scn = scenario("launchAnalysis100")
+		.feed(tsv("100_submissions_workspaceNames.tsv")) //feed the list of 100 clones created for this test
+		.exec(http("submission_request")
+			.post("/api/workspaces/broad-dsde-dev/${workspaceName}/submissions")
+			.headers(headers)
+			.body(StringBody(submissionBody))) //launch the same submission in each workspace
+
+	setUp(scn.inject(rampUsers(100) over(60 seconds))).protocols(httpProtocol) //ramp up 100 users over 60 seconds
+}
