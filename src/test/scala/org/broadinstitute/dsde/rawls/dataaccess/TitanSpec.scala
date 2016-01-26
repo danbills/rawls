1bd83471fb94957fd1d77383d290fd8d16ae8f9f
Hussein Elgridly <hussein@broadinstitute.org>
2016-01-26 13:24:55 -0500

    got aurelius code example throwing exception

--- src/test/scala/org/broadinstitute/dsde/rawls/dataaccess/TitanSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/rawls/dataaccess/TitanSpec.scala
@@ -27,7 +27,7 @@ class TitanSpec extends FlatSpec with Matchers {
     tx.commit()
   }
 
-  "Titan" should "do things" in {
+  def setupTitanGraph(): Graph = {
     val factory = TitanFactory.build()
     val tg: Graph = factory
       .set("storage.backend", "embeddedcassandra")
@@ -36,9 +36,6 @@ class TitanSpec extends FlatSpec with Matchers {
 
     tg.tx().onReadWrite(Transaction.READ_WRITE_BEHAVIOR.MANUAL)
     tg.tx().onClose(Transaction.CLOSE_BEHAVIOR.MANUAL)
-
-
-
 
     val mgmt = tg.asInstanceOf[TitanGraph].openManagement()
 
@@ -69,7 +66,106 @@ class TitanSpec extends FlatSpec with Matchers {
     v.property("namespace", "mynamespace")
     commit(tx)
 
+    tg
+  }
 
+  "Titan" should "do things with normal txns" in {
+    val tg = setupTitanGraph()
+
+    val t1 = new Thread(new Runnable {
+      def run(): Unit = {
+        val myTx = tg.tx()
+        myTx.open()
+        Thread.sleep(1000)
+        val wsVert = tg.traversal().V().hasLabel("Workspace").has("name", "myworkspace").has("namespace", "mynamespace").toList.head
+        println("currently: " + wsVert.property("beep"))
+        try {
+          println("beep boop: " + myTx)
+          wsVert.property("beep", "boop")
+        } catch {
+          case sve: SchemaViolationException => println("LOCKFAIL")
+        }
+
+        commit(myTx)
+      }
+    })
+
+    val t2 = new Thread(new Runnable {
+      def run(): Unit = {
+        val myTx = tg.tx()
+        myTx.open()
+        Thread.sleep(100)
+        val wsVert = tg.traversal().V().hasLabel("Workspace").has("name", "myworkspace").has("namespace", "mynamespace").toList.head
+        println("beep beep: " + myTx)
+        wsVert.property("beep", "beep")
+        Thread.sleep(100)
+        commit(myTx)
+      }
+    })
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    val tx2 = getTx(tg)
+    val wsVert = tg.traversal().V().hasLabel("Workspace").has("name", "myworkspace").has("namespace", "mynamespace").toList.head
+    wsVert.properties[Any]() foreach { vtxProp =>
+      println(vtxProp.key + ", " + vtxProp.value)
+    }
+    commit(tx2)
+  }
+
+  it should "do what titanfolks say" in {
+    val factory = TitanFactory.build()
+    val graph: Graph = factory
+      .set("storage.backend", "embeddedcassandra")
+      .set("storage.conf-file", "file:///c:/code/rawls/cassandra.yaml")
+      .open
+    
+    val mgmt = graph.asInstanceOf[TitanGraph].openManagement()
+    val name = mgmt.makePropertyKey("name").dataType(classOf[String]).make()
+    val index = mgmt.buildIndex("name", classOf[Vertex]).addKey(name).unique().buildCompositeIndex()
+    mgmt.setConsistency(index, ConsistencyModifier.LOCK)
+    mgmt.commit()
+
+    graph.addVertex("name","stephen")
+    graph.tx().commit()
+    val g = graph.traversal()
+    val l = new java.util.concurrent.CountDownLatch(1)
+    val t1 = new Thread( new Runnable {
+      def run() {
+        try {
+          def v = g.V().has("name","stephen").next()
+          v.property("name","marko")
+          l.await()
+          graph.tx().commit()
+        } catch {
+          case e: RuntimeException => println(e.getMessage); graph.tx().rollback()
+        }
+      }
+    })
+
+    val t2 = new Thread( new Runnable {
+      def run() {
+        def v = g.V().has("name", "stephen").next()
+        v.property("name", "daniel")
+        graph.tx().commit()
+        graph.tx().rollback()
+        l.countDown()
+      }
+    })
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+  }
+
+  it should "do things with threaded txns" in {
+    val tg = setupTitanGraph()
 
     val t1 = new Thread(new Runnable {
       def run(): Unit = {
@@ -115,4 +211,6 @@ class TitanSpec extends FlatSpec with Matchers {
     }
     commit(tx2)
   }
+
+
 }
