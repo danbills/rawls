b647fd3a00108ed73c2c472a7d2f93a2cf787d8f
Hussein Elgridly <hussein@broadinstitute.org>
2016-01-27 13:10:02 -0500

    got threaded txns and concurrency commit exceptions working

--- src/test/scala/org/broadinstitute/dsde/rawls/dataaccess/TitanSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/rawls/dataaccess/TitanSpec.scala
@@ -69,90 +69,53 @@ class TitanSpec extends FlatSpec with Matchers {
     tg
   }
 
-  "Titan" should "do things with normal txns" in {
-    val tg = setupTitanGraph()
-
-    val t1 = new Thread(new Runnable {
-      def run(): Unit = {
-        val myTx = tg.tx()
-        myTx.open()
-        Thread.sleep(1000)
-        val wsVert = tg.traversal().V().hasLabel("Workspace").has("name", "myworkspace").has("namespace", "mynamespace").toList.head
-        println("currently: " + wsVert.property("beep"))
-        try {
-          println("beep boop: " + myTx)
-          wsVert.property("beep", "boop")
-        } catch {
-          case sve: SchemaViolationException => println("LOCKFAIL")
-        }
-
-        commit(myTx)
-      }
-    })
-
-    val t2 = new Thread(new Runnable {
-      def run(): Unit = {
-        val myTx = tg.tx()
-        myTx.open()
-        Thread.sleep(100)
-        val wsVert = tg.traversal().V().hasLabel("Workspace").has("name", "myworkspace").has("namespace", "mynamespace").toList.head
-        println("beep beep: " + myTx)
-        wsVert.property("beep", "beep")
-        Thread.sleep(100)
-        commit(myTx)
-      }
-    })
-
-    t1.start()
-    t2.start()
-
-    t1.join()
-    t2.join()
-
-    val tx2 = getTx(tg)
-    val wsVert = tg.traversal().V().hasLabel("Workspace").has("name", "myworkspace").has("namespace", "mynamespace").toList.head
-    wsVert.properties[Any]() foreach { vtxProp =>
-      println(vtxProp.key + ", " + vtxProp.value)
-    }
-    commit(tx2)
-  }
-
-  it should "do what titanfolks say" in {
+  "Titan" should "throw an exception when the same key is being written to across two threaded txns" in {
     val factory = TitanFactory.build()
     val graph: Graph = factory
       .set("storage.backend", "embeddedcassandra")
       .set("storage.conf-file", "file:///c:/code/rawls/cassandra.yaml")
       .open
-    
+
+    graph.tx().onReadWrite(Transaction.READ_WRITE_BEHAVIOR.MANUAL) //can handle txns manually
+    graph.tx().onClose(Transaction.CLOSE_BEHAVIOR.MANUAL)
+
     val mgmt = graph.asInstanceOf[TitanGraph].openManagement()
     val name = mgmt.makePropertyKey("name").dataType(classOf[String]).make()
-    val index = mgmt.buildIndex("name", classOf[Vertex]).addKey(name).unique().buildCompositeIndex()
+    val index = mgmt.buildIndex("name", classOf[Vertex]).addKey(name).buildCompositeIndex() //doesn't have to be unique
     mgmt.setConsistency(index, ConsistencyModifier.LOCK)
     mgmt.commit()
 
+    val inittx = graph.tx()
+    inittx.open()
     graph.addVertex("name","stephen")
-    graph.tx().commit()
-    val g = graph.traversal()
+    inittx.commit()
+
     val l = new java.util.concurrent.CountDownLatch(1)
     val t1 = new Thread( new Runnable {
       def run() {
+        val txg = graph.tx().createThreadedTx[Graph]()
+        val mytx = txg.tx()
+        mytx.open()
         try {
-          def v = g.V().has("name","stephen").next()
+          def v = txg.traversal().V().has("name","stephen").next()
           v.property("name","marko")
           l.await()
-          graph.tx().commit()
+          mytx.commit()
         } catch {
-          case e: RuntimeException => println(e.getMessage); graph.tx().rollback()
+          case e: RuntimeException =>
+            println(e.getMessage)
         }
       }
     })
 
     val t2 = new Thread( new Runnable {
       def run() {
-        def v = g.V().has("name", "stephen").next()
+        val txg = graph.tx().createThreadedTx[Graph]()
+        val mytx = txg.tx()
+        mytx.open()
+        def v = txg.traversal().V().has("name", "stephen").next()
         v.property("name", "daniel")
-        graph.tx().commit()
-        graph.tx().rollback()
+        mytx.commit()
         l.countDown()
       }
     })
@@ -163,54 +126,5 @@ class TitanSpec extends FlatSpec with Matchers {
     t1.join()
     t2.join()
   }
-
-  it should "do things with threaded txns" in {
-    val tg = setupTitanGraph()
-
-    val t1 = new Thread(new Runnable {
-      def run(): Unit = {
-        val txGraph = tg.tx().createThreadedTx[Graph]()
-        val myTx = getTx(txGraph)
-        Thread.sleep(1000)
-        val wsVert = txGraph.traversal().V().hasLabel("Workspace").has("name", "myworkspace").has("namespace", "mynamespace").toList.head
-        println("currently: " + wsVert.property("beep"))
-        try {
-          println("beep boop: " + myTx)
-          wsVert.property("beep", "boop")
-        } catch {
-          case sve: SchemaViolationException => println("LOCKFAIL")
-        }
-
-        commit(myTx)
-      }
-    })
-
-    val t2 = new Thread(new Runnable {
-      def run(): Unit = {
-        val txGraph = tg.tx().createThreadedTx[Graph]()
-        val myTx = getTx(txGraph)
-        Thread.sleep(100)
-        val wsVert = txGraph.traversal().V().hasLabel("Workspace").has("name", "myworkspace").has("namespace", "mynamespace").toList.head
-        println("beep beep: " + myTx)
-        wsVert.property("beep", "beep")
-        Thread.sleep(100)
-        commit(myTx)
-      }
-    })
-
-    t1.start()
-    t2.start()
-
-    t1.join()
-    t2.join()
-
-    val tx2 = getTx(tg)
-    val wsVert = tg.traversal().V().hasLabel("Workspace").has("name", "myworkspace").has("namespace", "mynamespace").toList.head
-    wsVert.properties[Any]() foreach { vtxProp =>
-      println(vtxProp.key + ", " + vtxProp.value)
-    }
-    commit(tx2)
-  }
-
 
 }
