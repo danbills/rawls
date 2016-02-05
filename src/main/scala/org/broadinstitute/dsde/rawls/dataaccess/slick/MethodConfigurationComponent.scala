486521fa143e1cba1b48ed14663e7dbe2d0c9602
Matt Bemis <mbemis@broadinstitute.org>
2016-02-05 15:44:19 -0500

    pr feedback

--- src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/MethodConfigurationComponent.scala
+++ src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/MethodConfigurationComponent.scala
@@ -3,6 +3,7 @@ package org.broadinstitute.dsde.rawls.dataaccess.slick
 case class MethodConfigurationRecord(id: Long,
                                      namespace: String,
                                      name: String,
+                                     workspaceId: String,
                                      rootEntityType: String,
                                      methodNamespace: String,
                                      methodName: String,
@@ -23,14 +24,15 @@ trait MethodConfigurationComponent {
     def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
     def namespace = column[String]("NAMESPACE")
     def name = column[String]("NAME")
+    def workspaceId = column[String]("WORKSPACE_ID")
     def rootEntityType = column[String]("ROOT_ENTITY_TYPE")
     def methodNamespace = column[String]("METHOD_NAMESPACE")
     def methodName = column[String]("METHOD_NAME")
     def methodVersion = column[Int]("METHOD_VERSION")
 
-    def * = (id, namespace, name, rootEntityType, methodNamespace, methodName, methodVersion) <> (MethodConfigurationRecord.tupled, MethodConfigurationRecord.unapply)
+    def * = (id, namespace, name, workspaceId, rootEntityType, methodNamespace, methodName, methodVersion) <> (MethodConfigurationRecord.tupled, MethodConfigurationRecord.unapply)
 
-    def namespaceNameIdx = index("NAMESPACE_NAME_IDX", (namespace, name), unique = true)
+    def namespaceNameIdx = index("IDX_CONFIG", (namespace, name), unique = true)
   }
 
   class MethodConfigurationInputTable(tag: Tag) extends Table[MethodConfigurationInputRecord](tag, "METHOD_CONFIG_INPUT") {
@@ -41,8 +43,8 @@ trait MethodConfigurationComponent {
 
     def * = (methodConfigId, id, key, value) <> (MethodConfigurationInputRecord.tupled, MethodConfigurationInputRecord.unapply)
 
-    def methodConfig = foreignKey("METHOD_CONFIG_FK", methodConfigId, methodConfigurationQuery)(_.id)
-    def configKeyIdx = index("CONFIG_KEY_IDX", (methodConfigId, key), unique = true)
+    def methodConfig = foreignKey("FK_MC_INPUT", methodConfigId, methodConfigurationQuery)(_.id)
+    def configKeyIdx = index("IDX_MC_INPUT", (methodConfigId, key), unique = true)
   }
 
   class MethodConfigurationOutputTable(tag: Tag) extends Table[MethodConfigurationOutputRecord](tag, "METHOD_CONFIG_OUTPUT") {
@@ -53,8 +55,8 @@ trait MethodConfigurationComponent {
 
     def * = (methodConfigId, id, key, value) <> (MethodConfigurationOutputRecord.tupled, MethodConfigurationOutputRecord.unapply)
 
-    def methodConfig = foreignKey("METHOD_CONFIG_FK", methodConfigId, methodConfigurationQuery)(_.id)
-    def configKeyIdx = index("CONFIG_KEY_IDX", (methodConfigId, key), unique = true)
+    def methodConfig = foreignKey("FK_MC_OUTPUT", methodConfigId, methodConfigurationQuery)(_.id)
+    def configKeyIdx = index("IDX_MC_OUTPUT", (methodConfigId, key), unique = true)
   }
 
   class MethodConfigurationPrereqTable(tag: Tag) extends Table[MethodConfigurationPrereqRecord](tag, "METHOD_CONFIG_PREREQ") {
@@ -65,12 +67,52 @@ trait MethodConfigurationComponent {
 
     def * = (methodConfigId, id, key, value) <> (MethodConfigurationPrereqRecord.tupled, MethodConfigurationPrereqRecord.unapply)
 
-    def methodConfig = foreignKey("METHOD_CONFIG_FK", methodConfigId, methodConfigurationQuery)(_.id)
-    def configKeyIdx = index("CONFIG_KEY_IDX", (methodConfigId, key), unique = true)
+    def methodConfig = foreignKey("FK_MC_PREREQ", methodConfigId, methodConfigurationQuery)(_.id)
+    def configKeyIdx = index("IDX_MC_PREREQ", (methodConfigId, key), unique = true)
   }
 
   protected val methodConfigurationQuery = TableQuery[MethodConfigurationTable]
   protected val methodConfigurationInputQuery = TableQuery[MethodConfigurationInputTable]
   protected val methodConfigurationOutputQuery = TableQuery[MethodConfigurationOutputTable]
   protected val methodConfigurationPrereqQuery = TableQuery[MethodConfigurationPrereqTable]
+
+  def saveMethodConfiguration(config: MethodConfigurationRecord) = {
+    methodConfigurationQuery insertOrUpdate config map { _ => config }
+  }
+
+  def loadMethodConfiguration(namespace: String, name: String) = {
+    methodConfigurationQuery.filter(mc => (mc.namespace === namespace && mc.name === name)).result.head
+  }
+
+  def deleteMethodConfiguration(namespace: String, name: String) = {
+    methodConfigurationQuery.filter(mc => (mc.namespace === namespace && mc.name === name)).delete
+  }
+
+  def listMethodConfigurations(workspaceId: String) = {
+    methodConfigurationQuery.filter(_.workspaceId === workspaceId).result
+  }
+
+  def saveMethodConfigInput(input: MethodConfigurationInputRecord) = {
+    methodConfigurationInputQuery insertOrUpdate input map { _ => input }
+  }
+
+  def listMethodConfigInputs(configId: Long) = {
+    methodConfigurationInputQuery.filter(_.methodConfigId === configId).result
+  }
+
+  def saveMethodConfigOutput(output: MethodConfigurationOutputRecord) = {
+    methodConfigurationOutputQuery insertOrUpdate output map { _ => output }
+  }
+
+  def listMethodConfigOutputs(methodConfigId: Long) = {
+    methodConfigurationOutputQuery.filter(_.methodConfigId === methodConfigId).result
+  }
+
+  def saveMethodConfigPrereq(prereq: MethodConfigurationPrereqRecord) = {
+    methodConfigurationPrereqQuery insertOrUpdate prereq map { _ => prereq }
+  }
+
+  def listMethodConfigPrereqs(methodConfigId: Long) = {
+    methodConfigurationPrereqQuery.filter(_.methodConfigId === methodConfigId).result
+  }
 }
---
+++ src/test/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/MethodConfigurationComponentSpec.scala
@@ -0,0 +1,99 @@
+package org.broadinstitute.dsde.rawls.dataaccess.slick
+
+import org.scalatest.{BeforeAndAfterAll, Matchers, FlatSpec}
+
+class MethodConfigurationComponentSpec extends FlatSpec with Matchers with TestDriverComponent with MethodConfigurationComponent with BeforeAndAfterAll {
+  import driver.api._
+
+  val schemas = methodConfigurationQuery.schema ++
+    methodConfigurationInputQuery.schema //++
+//    methodConfigurationOutputQuery.schema ++
+//    methodConfigurationPrereqQuery.schema
+
+  override def beforeAll: Unit = {
+    runAndWait(schemas.create)
+  }
+
+  "MethodConfigurationComponent" should "create, load, list and delete" in {
+
+    val methodConfig = MethodConfigurationRecord(1, "test", "foo", "1", "sample", "testns", "testname", 1)
+
+    assertResult(Seq()) {
+      runAndWait(listMethodConfigurations("1"))
+    }
+
+    assertResult(methodConfig) {
+      runAndWait(saveMethodConfiguration(methodConfig))
+    }
+
+    assertResult(methodConfig) {
+      runAndWait(loadMethodConfiguration("test", "foo"))
+    }
+
+    assertResult(Seq(methodConfig)) {
+      runAndWait(listMethodConfigurations("1"))
+    }
+
+    assertResult(1) {
+      runAndWait(deleteMethodConfiguration("test", "foo"))
+    }
+
+    assertResult(Seq()) {
+      runAndWait(listMethodConfigurations("1"))
+    }
+
+  }
+
+  it should "list method config inputs, outputs, and prereqs for a method config" in {
+
+    val methodConfig = MethodConfigurationRecord(1, "test", "foo", "1", "sample", "testns", "testname", 1)
+    val input = MethodConfigurationInputRecord(1, 1, "test", "input")
+    val output = MethodConfigurationOutputRecord(1, 1, "test", "output")
+    val prereq = MethodConfigurationPrereqRecord(1, 1, "test", "prereq")
+
+    assertResult(methodConfig) {
+      runAndWait(saveMethodConfiguration(methodConfig))
+    }
+
+    //inputs
+    assertResult(Seq()) {
+      runAndWait(listMethodConfigInputs(1))
+    }
+
+    assertResult(input) {
+      runAndWait(saveMethodConfigInput(input))
+    }
+
+    assertResult(Seq(input)) {
+      runAndWait(listMethodConfigInputs(1))
+    }
+
+    //outputs
+    assertResult(Seq()) {
+      runAndWait(listMethodConfigOutputs(1))
+    }
+
+    assertResult(output) {
+      runAndWait(saveMethodConfigOutput(output))
+    }
+
+    assertResult(Seq(output)) {
+      runAndWait(listMethodConfigOutputs(1))
+    }
+
+    //prereqs
+    assertResult(Seq()) {
+      runAndWait(listMethodConfigPrereqs(1))
+    }
+
+    assertResult(prereq) {
+      runAndWait(saveMethodConfigPrereq(prereq))
+    }
+
+    assertResult(Seq(prereq)) {
+      runAndWait(listMethodConfigPrereqs(1))
+    }
+
+
+  }
+}
