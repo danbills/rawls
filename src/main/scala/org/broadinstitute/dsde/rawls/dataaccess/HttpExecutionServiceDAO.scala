725d85fcead300d4bdfe9bd98cedc34942d716a8
Hussein Elgridly <hussein@broadinstitute.org>
2016-08-01 15:37:45 -0400

    log some of the things

--- src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/HttpExecutionServiceDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/HttpExecutionServiceDAO.scala
@@ -2,13 +2,15 @@ package org.broadinstitute.dsde.rawls.dataaccess
 
 import akka.actor.ActorSystem
 import akka.util.Timeout
+import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.rawls.model._
 import org.broadinstitute.dsde.rawls.model.ExecutionJsonSupport._
 import org.broadinstitute.dsde.rawls.util.FutureSupport
+
 import scala.concurrent.Future
 import scala.concurrent.duration.FiniteDuration
 import spray.client.pipelining._
-import spray.http.FormData
+import spray.http.{FormData, HttpRequest}
 import spray.httpx.SprayJsonSupport._
 
 import scala.util.Try
@@ -16,14 +18,17 @@ import scala.util.Try
 /**
  * @author tsharpe
  */
-class HttpExecutionServiceDAO( executionServiceURL: String, submissionTimeout: FiniteDuration )( implicit val system: ActorSystem ) extends ExecutionServiceDAO with DsdeHttpDAO with Retry with FutureSupport {
+class HttpExecutionServiceDAO( executionServiceURL: String, submissionTimeout: FiniteDuration )( implicit val system: ActorSystem ) extends ExecutionServiceDAO with DsdeHttpDAO with Retry with FutureSupport with LazyLogging {
+
+  val logRequest: HttpRequest => HttpRequest = { req => logger.info(s"%s rq %s".format(this, req.toString())); req}
+  val logResponse: HttpRequest => HttpRequest = { resp => logger.info(s"%s resp %s".format(this, resp.toString())); resp}
 
   override def submitWorkflows(wdl: String, inputs: Seq[String], options: Option[String], userInfo: UserInfo): Future[Seq[Either[ExecutionServiceStatus, ExecutionServiceFailure]]] = {
     implicit val timeout = Timeout(submissionTimeout)
     val url = executionServiceURL+"/workflows/v1/batch"
     import system.dispatcher
 
-    val pipeline = addAuthHeader(userInfo) ~> sendReceive ~> unmarshal[Seq[Either[ExecutionServiceStatus, ExecutionServiceFailure]]]
+    val pipeline = addAuthHeader(userInfo) ~> logRequest ~> sendReceive ~> logResponse ~> unmarshal[Seq[Either[ExecutionServiceStatus, ExecutionServiceFailure]]]
     val formData = FormData(Seq("wdlSource" -> wdl, "workflowInputs" -> inputs.mkString("[", ",", "]")) ++ options.map("workflowOptions" -> _).toSeq)
     pipeline(Post(url,formData))
   }
@@ -31,35 +36,35 @@ class HttpExecutionServiceDAO( executionServiceURL: String, submissionTimeout: F
   override def status(id: String, userInfo: UserInfo): Future[ExecutionServiceStatus] = {
     val url = executionServiceURL + s"/workflows/v1/${id}/status"
     import system.dispatcher
-    val pipeline = addAuthHeader(userInfo) ~> sendReceive ~> unmarshal[ExecutionServiceStatus]
+    val pipeline = addAuthHeader(userInfo) ~> logRequest ~> sendReceive ~> logResponse ~> unmarshal[ExecutionServiceStatus]
     retry(when500) { () => pipeline(Get(url)) }
   }
 
   override def callLevelMetadata(id: String, userInfo: UserInfo): Future[ExecutionMetadata] = {
     val url = executionServiceURL + s"/workflows/v1/${id}/metadata"
     import system.dispatcher
-    val pipeline = addAuthHeader(userInfo) ~> sendReceive ~> unmarshal[ExecutionMetadata]
+    val pipeline = addAuthHeader(userInfo) ~> logRequest ~> sendReceive ~> logResponse ~> unmarshal[ExecutionMetadata]
     retry(when500) { () => pipeline(Get(url)) }
   }
 
   override def outputs(id: String, userInfo: UserInfo): Future[ExecutionServiceOutputs] = {
     val url = executionServiceURL + s"/workflows/v1/${id}/outputs"
     import system.dispatcher
-    val pipeline = addAuthHeader(userInfo) ~> sendReceive ~> unmarshal[ExecutionServiceOutputs]
+    val pipeline = addAuthHeader(userInfo) ~> logRequest ~> sendReceive ~> logResponse ~> unmarshal[ExecutionServiceOutputs]
     retry(when500) { () => pipeline(Get(url)) }
   }
 
   override def logs(id: String, userInfo: UserInfo): Future[ExecutionServiceLogs] = {
     val url = executionServiceURL + s"/workflows/v1/${id}/logs"
     import system.dispatcher
-    val pipeline = addAuthHeader(userInfo) ~> sendReceive ~> unmarshal[ExecutionServiceLogs]
+    val pipeline = addAuthHeader(userInfo) ~> logRequest ~> sendReceive ~> logResponse ~> unmarshal[ExecutionServiceLogs]
     retry(when500) { () => pipeline(Get(url)) }
   }
 
   override def abort(id: String, userInfo: UserInfo): Future[Try[ExecutionServiceStatus]] = {
     val url = executionServiceURL + s"/workflows/v1/${id}/abort"
     import system.dispatcher
-    val pipeline = addAuthHeader(userInfo) ~> sendReceive ~> unmarshal[ExecutionServiceStatus]
+    val pipeline = addAuthHeader(userInfo) ~> logRequest ~> sendReceive ~> logResponse ~> unmarshal[ExecutionServiceStatus]
     retry(when500) { () => toFutureTry(pipeline(Post(url))) }
   }
 
